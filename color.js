3b2cf765aa4421bcbc309cfb656e4cb620315e35
joshr19 <89062549+joshr19@users.noreply.github.com>
2021-08-17 18:49:56 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,21 @@
+function makered()
+{
+    document.body.style.backgroundColor="red";
+}
+
+
+function makeblue()
+{
+    document.body.style.backgroundColor="blue";
+}
+function makeorange()
+{
+    document.body.style.backgroundColor="orange";
+}
+function makeyellow()
+{
+    document.body.style.backgroundColor="yellow";
+}
+
+
+
